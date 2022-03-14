eccc911cd71f9cef5d5e13e361411a2c18b73232
d.vasiliev <d.vasiliev@mixvel.com>
2022-03-14 19:48:54 +0300

    Created Vue-project. Implemented vue form for displaying all transactions.

---
+++ fba_analyst_front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+   <AllTransactions/>
+</template>
+
+<script>
+import AllTransactions from './components/AllTransactions.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AllTransactions
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fba_analyst_front/src/components/AllTransactions.vue
@@ -0,0 +1,45 @@
+<template>
+    <button @click="get()">Get data</button>
+    <div v-if="info !== null">
+        <table>
+            <tr>
+                <th>id</th>
+                <th>user</th>
+                <th>date &amp; time</th>
+                <th>amount</th>
+                <th>income</th>
+            </tr>
+            <tr v-for="el in info" :key="el.id">
+                <td>{{el.id}}</td>
+                <td>{{el.user}}</td>
+                <td>{{el.dateTime}}</td>
+                <td>{{el.amount}}</td>
+                <td>{{el.isIncome}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  name: 'AllTransactions',
+  props: {
+  },
+  data(){
+      return{
+          info: []
+      }
+  },
+  methods:{
+      get(){
+        axios
+        .get('https://localhost:7217/api/Transaction')
+        .then(response => {(this.info = response.data); console.log(this.info);});
+        
+      }
+  }
+
+}
+</script>
